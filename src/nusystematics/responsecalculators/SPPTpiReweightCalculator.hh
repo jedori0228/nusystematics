cbb4621d4ed42cf675c749b80596808201ddf0c7
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2023-12-14 11:02:16 -0600

    PiSyst values updated

--- src/nusystematics/responsecalculators/SPPTpiReweightCalculator.hh
+++ src/nusystematics/responsecalculators/SPPTpiReweightCalculator.hh
@@ -39,18 +39,18 @@ inline double GetSPPQ2Reweight(double Q2_GeV2, double parameter_value){
   if(X>=3.000000) X = 3.000000;
 
   double this_rw = 1.;
-  if( Q2_GeV2 < 0.025000) this_rw = 1.143409;
-  else if( X >= 0.025000 && Q2_GeV2 < 0.050000) this_rw = 1.384572;
-  else if( X >= 0.050000 && Q2_GeV2 < 0.100000) this_rw = 1.523289;
-  else if( X >= 0.100000 && Q2_GeV2 < 0.200000) this_rw = 1.504776;
-  else if( X >= 0.200000 && Q2_GeV2 < 0.300000) this_rw = 1.530361;
-  else if( X >= 0.300000 && Q2_GeV2 < 0.400000) this_rw = 1.477627;
-  else if( X >= 0.400000 && Q2_GeV2 < 0.500000) this_rw = 1.559800;
-  else if( X >= 0.500000 && Q2_GeV2 < 0.700000) this_rw = 1.389166;
-  else if( X >= 0.700000 && Q2_GeV2 < 1.000000) this_rw = 1.373498;
-  else if( X >= 1.000000 && Q2_GeV2 < 1.300000) this_rw = 1.215221;
-  else if( X >= 1.300000 && Q2_GeV2 < 2.000000) this_rw = 1.044024;
-  else if( X >= 2.000000 && Q2_GeV2 < 3.000000) this_rw = 1.574511;
+  if( X < 0.025000) this_rw = 1.254887;
+  else if( X >= 0.025000 && X < 0.050000) this_rw = 1.589738;
+  else if( X >= 0.050000 && X < 0.100000) this_rw = 1.735876;
+  else if( X >= 0.100000 && X < 0.200000) this_rw = 1.654647;
+  else if( X >= 0.200000 && X < 0.300000) this_rw = 1.663086;
+  else if( X >= 0.300000 && X < 0.400000) this_rw = 1.586161;
+  else if( X >= 0.400000 && X < 0.500000) this_rw = 1.707708;
+  else if( X >= 0.500000 && X < 0.700000) this_rw = 1.478242;
+  else if( X >= 0.700000 && X < 1.000000) this_rw = 1.460229;
+  else if( X >= 1.000000 && X < 1.300000) this_rw = 1.255551;
+  else if( X >= 1.300000 && X < 2.000000) this_rw = 1.053312;
+  else if( X >= 2.000000 && X < 3.000000) this_rw = 1.659176;
   else{
     this_rw = 1.;
   }
